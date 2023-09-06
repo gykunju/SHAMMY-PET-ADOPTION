150ba888e571e4e58ec27583f4bc7d7c4d70af0a
samuel <samuelkelvinmwangi@gmail.com>
2023-09-06 22:51:56 +0300

    making changes to the pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,40 +2,40 @@ class PetsController < ApplicationController
     before_action :set_pet, only: [:show, :update, :destroy]
 
   def index
-    @pets = Pet.all
-    render json: @pets
+    pets = Pet.all
+    render json: pets
   end
 
   def show
-    render json: @pet
+    render json: pet
   end
 
   def create
-    @pet = Pet.new(pet_params)
+    pet = Pet.new(pet_params)
 
-    if @pet.save
-      render json: @pet, status: :created, location: @pet
+    if pet.save
+      render json: pet, status: :created, location: pet
     else
-      render json: @pet.errors, status: :unprocessable_entity
+      render json: pet.errors, status: :unprocessable_entity
     end
   end
 
   def update
-    if @pet.update(pet_params)
-      render json: @pet
+    if pet.update(pet_params)
+      render json: pet
     else
-      render json: @pet.errors, status: :unprocessable_entity
+      render json: pet.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @pet.destroy
+    pet.destroy
   end
 
   private
 
   def set_pet
-    @pet = Pet.find(params[:id])
+    pet = Pet.find(params[:id])
   end
 
   def pet_params
