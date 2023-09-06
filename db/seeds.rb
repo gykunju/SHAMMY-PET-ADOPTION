4a9e6bf7dd968e8dfb5eac6da8b6813b5ddb11e6
samuel <samuelkelvinmwangi@gmail.com>
2023-09-06 21:21:00 +0300

    adding faker code for generation of data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do
+    adopter = Adopter.create(
+      name: Faker::Name.name,
+      contact_info: Faker::PhoneNumber.phone_number,
+      email: Faker::Internet.email
+    )
+  
+    5.times do
+      Pet.create(
+        name: Faker::Creature::Name.name,
+        species: Faker::Creature::Animal.name,
+        age: Faker::Number.between(from: 1, to: 10),
+        image: Faker::LoremPixel.image(size: '300x300', is_gray: false, category: 'animals'),
+        description: Faker::Lorem.sentence,
+        adopter: adopter
+      )
+    end
+  end
