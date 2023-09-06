a08b30e97162872dc9ae35665fd289219f437891
cynthia <cynthiakagz@gmail.com>
2023-09-07 00:35:56 +0300

    pet controller

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,18 @@
+class PetsController < ApplicationController
+    def index 
+        render json: Pet.all
+    end
+    def show 
+        pet = Pet.find(params[:id])
+        render json: pet
+    end
+    def create 
+        pet = Pet.create!(pet_params)
+        render json: pet
+    rescue ActiveRecord::RecordInvalid => e
+     render json: {error: e.error.full_messages}, status: :unprocessable_entity
+
+    end
+    private
+    def 
+end
