00dda263f788045c9a3b7dbceded5718011c0b29
Sharon Awuor <sawuor395@gmail.com>
2023-09-07 09:43:23 +0300

    pets controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  get 'users', to: 'users#index'
+  # devise_for :users, controllers: { registrations: 'registrations' }
+  resources :pets, only: [:index, :show]
+  post 'signup', to: 'users#create'
+  post 'login', to: 'users#show'
+
+  # Define separate routes for admin actions (create, update, destroy)
+  namespace :admin do
+    resources :pets, only: [:create, :update, :destroy]
+  end
 end
+
+
