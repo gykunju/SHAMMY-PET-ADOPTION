39b04aab1b0aae2566354c3fe5d80aef5cdfcf98
Sharonne24 <sawuor395@gmail.com>
2023-09-07 10:44:08 +0300

    Add files via upload

---
+++ routes.rb
@@ -0,0 +1,15 @@
+
+Rails.application.routes.draw do
+  get 'users', to: 'users#index'
+  # devise_for :users, controllers: { registrations: 'registrations' }
+  resources :pets, only: [:index, :show]
+  post 'signup', to: 'users#create'
+  post 'login', to: 'users#show'
+
+  # Define separate routes for admin actions (create, update, destroy)
+  namespace :admin do
+    resources :pets, only: [:create, :update, :destroy]
+  end
+end
+
+
