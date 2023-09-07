feb5a5c08713db8cd19f0037b8ff76900c44f7de
gykunju <alvingikunju@gmail.com>
2023-09-07 10:16:37 +0300

    retouch on show and create

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -4,10 +4,21 @@ class AdoptionsController < ApplicationController
         render json: Adoption.all
     end
 
+    def show
+        adoption = Adoption.find_by(params[:id])
+        render json: adoption
+    end
+
     def create 
         adoption = Adoption.create!(adoption_params)
         render json: adoption
     rescue ActiveRecord::RecordInvalid => e
         render json: {error: e.error.full_message}
+    end
+
+    private
+
+    def adoption_params
+        params.permit(:location, :contact, :reason, :user_id, :pet_id)
     end
 end
