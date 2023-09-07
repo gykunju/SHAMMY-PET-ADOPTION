efe5e7af0ae8f641446aa02982ef9692e7d4f80c
Sharonne24 <sawuor395@gmail.com>
2023-09-07 10:41:47 +0300

    Add files via upload

---
+++ seeds.rb
@@ -0,0 +1,21 @@
+User.destroy_all
+# Create an admin user
+admin_user = User.create(
+  user_name: 'sharon_username',
+  email: 'sharon@example.com',
+  password: 'password1',
+  admin: true
+)
+
+# Create the second admin user
+admin_user2 = User.create(
+  user_name: 'cynthia_username',
+  email: 'cynthia@example.com',
+  password: 'password2',
+  admin: true
+)
+
+
+puts 'Admin user created.'
+
+
