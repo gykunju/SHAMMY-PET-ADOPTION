c05128fbf3a0f72bcbb34a55019339eb2b477f83
gykunju <alvingikunju@gmail.com>
2023-09-07 10:01:33 +0300

    add show action and validation

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -4,4 +4,10 @@ class AdoptionsController < ApplicationController
         render json: Adoption.all
     end
 
+    def create 
+        adoption = Adoption.create!(adoption_params)
+        render json: adoption
+    rescue ActiveRecord::RecordInvalid => e
+        render json: {error: e.error.full_message}
+    end
 end
--- app/models/adoption.rb
+++ app/models/adoption.rb
@@ -1,2 +1,6 @@
 class Adoption < ApplicationRecord
+
+    validates :contact, presence: true
+    validates :location, presence: true
+    validates :reason, presence: true
 end
